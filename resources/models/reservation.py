595a2867733326a0bbf624c64c4763179408eb36
Deepak Panta <deepak.panta@anders.fi>
2022-07-14 15:38:29 +0300

    Allow reservation owners to cancel manually confirmed reservation
    Allow users to cancel reservation that are manuallly confirmed if:
    * The resource allows to cancel the manual reservation
    * The day of reservation is within the range of cancellation days.
      i.e. If the cancellation_min_days_in_advance is 2 and the
      reservation will starts before or equal to 2 days.
    
    Refs TAM-148

--- resources/models/reservation.py
+++ resources/models/reservation.py
@@ -279,7 +279,7 @@ class Reservation(ModifiableModel):
         self.state = new_state
         self.save()
 
-    def can_modify(self, user):
+    def can_modify(self, user, request_method=None):
         if not user:
             return False
 
@@ -290,10 +290,25 @@ class Reservation(ModifiableModel):
             return self.resource.can_modify_paid_reservations(user)
 
         # reservations that need manual confirmation and are confirmed cannot be
-        # modified or cancelled without reservation approve permission
-        cannot_approve = not self.resource.can_approve_reservations(user)
-        if self.need_manual_confirmation() and self.state == Reservation.CONFIRMED and cannot_approve:
-            return False
+        # modified or cancelled without reservation approve permission. However, if
+        # resource allows cancelling and the if cancelled before the allowed number of
+        # days, it's possible to cancel the reservation for resource that are free i.e.
+        # The ones that can be used without paying.
+        if self.need_manual_confirmation() and self.state == Reservation.CONFIRMED:
+            cancellation_min_days_in_advance = self.resource.cancellation_min_days_in_advance
+            cancellation_date_in_range = (self.begin - datetime.datetime.now(timezone.utc)).days >= cancellation_min_days_in_advance
+            cannot_approve = not self.resource.can_approve_reservations(user)
+            can_cancel_manually_confirmed_reservation = self.resource.owner_can_cancel_reservation
+
+            if (
+                request_method == 'DELETE'
+                and cancellation_date_in_range
+                and can_cancel_manually_confirmed_reservation
+                and self.resource.free_to_use
+            ):
+                return True
+            if cannot_approve:
+                return False
 
         return self.user == user or self.resource.can_modify_reservations(user)
 
--- respa_admin/forms.py
+++ respa_admin/forms.py
@@ -248,12 +248,15 @@ class ResourceForm(forms.ModelForm):
             'slot_size',
             'reservable_max_days_in_advance',
             'reservable_min_days_in_advance',
+            'cancellation_min_days_in_advance',
+            'owner_can_cancel_reservation',
             'max_reservations_per_user',
             'reservable',
             'should_be_reserved_whole_day',
             'need_manual_confirmation',
             'authentication',
             'access_code_type',
+            'free_to_use',
             'max_price',
             'min_price',
             'price_type',
@@ -276,6 +279,9 @@ class ResourceForm(forms.ModelForm):
                 choices=(thirty_minute_increment_choices)
             ),
             'need_manual_confirmation': RespaRadioSelect(
+                choices=((True, _('Yes')), (False, _('No')))
+            ),
+            'free_to_use': RespaRadioSelect(
                 choices=((True, _('Yes')), (False, _('No')))
             ),
             'public': forms.Select(
