21cc6baef94c9569a61a8a9f634fbcf04876bb0b
Joonas Syysvirta <joonas.syysvirta@anders.fi>
2023-03-17 10:23:39 +0200

    Add price list views to Respa Admin
    - Price list list view
    - Price list delete confirmation view
    - Base for price list edit/create form view
    
    Co-Authored-By: Deepak Panta <deepak.panta@anders.fi>
    
    Refs TTV-11, TTV-14

--- respa_admin/urls.py
+++ respa_admin/urls.py
@@ -12,6 +12,7 @@ from .views.resources import (
     SaveResourceView,
 )
 from .views.units import UnitEditView, UnitListView
+from .views.prices import PriceListView, PriceListCreateView, PriceListEditView, PriceListDeleteView
 
 app_name = 'respa_admin'
 urlpatterns = [
@@ -26,6 +27,10 @@ urlpatterns = [
     url(r'^resource/copy/(?P<resource_id>\w+)/$', copy_resource, name='copy-resource'),
     url(r'^units/$', UnitListView.as_view(), name='units'),
     url(r'^units/edit/(?P<unit_id>[\w\d:]+)/$', UnitEditView.as_view(), name='edit-unit'),
+    url(r'^price_list/$', PriceListView.as_view(), name='price-list'),
+    url(r'^price_list/edit/(?P<price_list_id>\w+)/$', PriceListEditView.as_view(), name='edit-price-list'),
+    url(r'^price_list/delete/(?P<price_list_id>\w+)/$', PriceListDeleteView.as_view(), name='delete-price-list'),
+    url(r'^price_list/new/$', PriceListCreateView.as_view(), name='new-price-list'),
     url(r'^i18n/$', include('django.conf.urls.i18n'), name='language'),
     url(r'^user_management/$', ManageUserPermissionsListView.as_view(), name='user-management'),
     url(r'^user_management/search/$', ManageUserPermissionsSearchView.as_view(), name='user-management-search'),
