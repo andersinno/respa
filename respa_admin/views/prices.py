9dd989f56d6ad196ed958c2cc3edf7536f356cd7
Dan Jacob <dan.jacob@anders.com>
2023-04-27 08:15:27 +0300

    Respa admin: tidy up views.py module
    Formatted module with Black and isort.
    
    Refs TTVA-54

--- respa_admin/views/prices.py
+++ respa_admin/views/prices.py
@@ -1,34 +1,34 @@
-from django.db.models import FieldDoesNotExist
 from django.core.exceptions import PermissionDenied
+from django.db.models import FieldDoesNotExist
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-from respa_pricing.models import PriceList
+from respa_admin.views.base import ExtraContextMixin
 from respa_pricing.forms import (
-    PriceListForm,
     EventTypePriceListItemFormset,
+    PriceListForm,
     UserGroupPriceListItemFormset,
 )
-from respa_admin.views.base import ExtraContextMixin
+from respa_pricing.models import PriceList
 
 
 class PriceListView(ExtraContextMixin, ListView):
     model = PriceList
     paginate_by = 10
-    context_object_name = 'price_lists'
-    template_name = 'respa_admin/page_price_lists.html'
+    context_object_name = "price_lists"
+    template_name = "respa_admin/page_price_lists.html"
 
     def get(self, request, *args, **kwargs):
         get_params = request.GET
-        self.search_query = get_params.get('search_query')
-        self.order_by = get_params.get('order_by', 'name')
+        self.search_query = get_params.get("search_query")
+        self.order_by = get_params.get("order_by", "name")
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['search_query'] = self.search_query or ''
-        context['order_by'] = self.order_by
+        context["search_query"] = self.search_query or ""
+        context["order_by"] = self.order_by
         return context
 
     def get_queryset(self):
@@ -37,7 +37,7 @@ class PriceListView(ExtraContextMixin, ListView):
         if self.search_query:
             qs = qs.filter(name__icontains=self.search_query)
         if self.order_by:
-            order_by_param = self.order_by.strip('-')
+            order_by_param = self.order_by.strip("-")
             try:
                 if PriceList._meta.get_field(order_by_param):
                     qs = qs.order_by(self.order_by)
@@ -48,9 +48,9 @@ class PriceListView(ExtraContextMixin, ListView):
 
 class PriceListCreateView(ExtraContextMixin, CreateView):
     model = PriceList
-    pk_url_kwarg = 'price_list_id'
+    pk_url_kwarg = "price_list_id"
     form_class = PriceListForm
-    template_name = 'respa_admin/price_lists/price_list_form.html'
+    template_name = "respa_admin/price_lists/price_list_form.html"
     success_url = reverse_lazy("respa_admin:price-list")
 
     def get(self, request, *args, **kwargs):
@@ -71,9 +71,9 @@ class PriceListCreateView(ExtraContextMixin, CreateView):
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
         resource_id = self.request.GET.get("resource_id")
-        form.fields["resource"].queryset = form.fields["resource"].queryset.modifiable_by(
-            self.request.user
-        )
+        form.fields["resource"].queryset = form.fields[
+            "resource"
+        ].queryset.modifiable_by(self.request.user)
         form.fields["resource"].initial = resource_id
         return form
 
@@ -88,8 +88,11 @@ class PriceListCreateView(ExtraContextMixin, CreateView):
         user_group_item_formset = UserGroupPriceListItemFormset(self.request.POST)
         event_type_item_formset = EventTypePriceListItemFormset(self.request.POST)
 
-        if (form.is_valid() and user_group_item_formset.is_valid() and
-            event_type_item_formset.is_valid()):
+        if (
+            form.is_valid()
+            and user_group_item_formset.is_valid()
+            and event_type_item_formset.is_valid()
+        ):
             return self.form_valid(
                 form,
                 user_group_item_formset,
@@ -97,9 +100,7 @@ class PriceListCreateView(ExtraContextMixin, CreateView):
             )
         else:
             return self.form_invalid(
-                form,
-                user_group_item_formset,
-                event_type_item_formset
+                form, user_group_item_formset, event_type_item_formset
             )
 
     def form_valid(self, form, user_group_item_formset, event_type_item_formset):
@@ -122,9 +123,9 @@ class PriceListCreateView(ExtraContextMixin, CreateView):
 
 class PriceListEditView(ExtraContextMixin, UpdateView):
     model = PriceList
-    pk_url_kwarg = 'price_list_id'
+    pk_url_kwarg = "price_list_id"
     form_class = PriceListForm
-    template_name = 'respa_admin/price_lists/price_list_form.html'
+    template_name = "respa_admin/price_lists/price_list_form.html"
     success_url = reverse_lazy("respa_admin:price-list")
 
     def get(self, request, *args, **kwargs):
@@ -144,9 +145,9 @@ class PriceListEditView(ExtraContextMixin, UpdateView):
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        form.fields["resource"].queryset = form.fields["resource"].queryset.modifiable_by(
-            self.request.user
-        )
+        form.fields["resource"].queryset = form.fields[
+            "resource"
+        ].queryset.modifiable_by(self.request.user)
         return form
 
     def get_queryset(self):
@@ -157,11 +158,18 @@ class PriceListEditView(ExtraContextMixin, UpdateView):
         self.object = self.get_object()
         form_class = self.get_form_class()
         form = self.get_form(form_class)
-        user_group_item_formset = UserGroupPriceListItemFormset(self.request.POST, instance=self.object)
-        event_type_item_formset = EventTypePriceListItemFormset(self.request.POST, instance=self.object)
+        user_group_item_formset = UserGroupPriceListItemFormset(
+            self.request.POST, instance=self.object
+        )
+        event_type_item_formset = EventTypePriceListItemFormset(
+            self.request.POST, instance=self.object
+        )
 
-        if (form.is_valid() and user_group_item_formset.is_valid() and
-            event_type_item_formset.is_valid()):
+        if (
+            form.is_valid()
+            and user_group_item_formset.is_valid()
+            and event_type_item_formset.is_valid()
+        ):
             return self.form_valid(
                 form,
                 user_group_item_formset,
@@ -169,9 +177,7 @@ class PriceListEditView(ExtraContextMixin, UpdateView):
             )
         else:
             return self.form_invalid(
-                form,
-                user_group_item_formset,
-                event_type_item_formset
+                form, user_group_item_formset, event_type_item_formset
             )
 
     def form_valid(self, form, user_group_item_formset, event_type_item_formset):
@@ -193,7 +199,7 @@ class PriceListEditView(ExtraContextMixin, UpdateView):
 
 
 class PriceListDeleteView(ExtraContextMixin, DeleteView):
-    """ A view to remove a price list """
+    """A view to remove a price list"""
 
     model = PriceList
     template_name = "respa_admin/price_lists/price_list_confirm_delete.html"
