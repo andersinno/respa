be9c9b2f8cc6fdd4613a418fdd1158940f030aa8
Joonas Syysvirta <joonas.syysvirta@anders.fi>
2023-05-02 10:33:11 +0300

    Add option to enable django-debug-toolbar
    Added DEBUG_TOOLBAR ocnfiguration option for enabling
    Django Debug Toolbar for development purposes.
    
    Refs TTVA-66

--- respa/urls.py
+++ respa/urls.py
@@ -50,3 +50,8 @@ if settings.RESPA_PAYMENTS_ENABLED:
     ])
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if "debug_toolbar" in settings.INSTALLED_APPS:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
