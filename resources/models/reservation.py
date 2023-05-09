8b96e30b6033ec1d5472bdb315690dddb6ed62bb
Joonas Syysvirta <joonas.syysvirta@anders.fi>
2023-05-09 14:18:17 +0300

    Set approved_at and requested_at on Reservation state change
    - requested_at is set when the reservation state changes
    to REQUESTED.
    - approved_at is set when the reservations is approved, i.e.
    when the state is set from REQUESTED to CONFIRMED or
    WAITING_FOR_PAYMENT (paid reservations).
    
    Refs TTVA-50

--- resources/models/reservation.py
+++ resources/models/reservation.py
@@ -364,12 +364,20 @@ class Reservation(ModifiableModel):
                 )
             self.send_reservation_changed_mail_to_user()
             return
+        
+        if new_state == Reservation.REQUESTED:
+            self.requested_at = timezone.now()
+
+        if new_state in (Reservation.CONFIRMED, Reservation.WAITING_FOR_PAYMENT):
+            if old_state == Reservation.REQUESTED:
+                self.approver = user
+                self.approved_at = timezone.now()
 
         if new_state == Reservation.CONFIRMED:
-            self.approver = user
             reservation_confirmed.send(sender=self.__class__, instance=self, user=user)
         elif old_state == Reservation.CONFIRMED:
             self.approver = None
+            self.approved_at = None
 
         user_is_staff = self.user is not None and self.user.is_staff
 
