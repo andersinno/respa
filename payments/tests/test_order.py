85920f71b4e5a9245d1dc2905d6f5f75ed920ab1
Dan Jacob <dan.jacob@anders.com>
2023-05-10 11:50:02 +0300

    Payments: tidy up models.py and test code
    Formatted code with isort/Black.
    
    Refs TTVA-51

--- payments/tests/test_order.py
+++ payments/tests/test_order.py
@@ -21,17 +21,24 @@ def test_get_price_correct(order_with_products):
     has an hourly rate and one is with a fixed price, plus individual product
     tax of 24% should equal 37.20"""
     price = order_with_products.get_price()
-    assert price == Decimal('37.20')
+    assert price == Decimal("37.20")
 
 
-@pytest.mark.parametrize('order_state, expected_reservation_state', (
-    (Order.CONFIRMED, Reservation.CONFIRMED),
-    (Order.REJECTED, Reservation.CANCELLED),
-    (Order.EXPIRED, Reservation.CANCELLED),
-    (Order.CANCELLED, Reservation.CANCELLED),
-))
-def test_set_state_sets_reservation_state(two_hour_reservation, order_state, expected_reservation_state):
-    old_order_state = Order.CONFIRMED if order_state == Order.CANCELLED else Order.WAITING
+@pytest.mark.parametrize(
+    "order_state, expected_reservation_state",
+    (
+        (Order.CONFIRMED, Reservation.CONFIRMED),
+        (Order.REJECTED, Reservation.CANCELLED),
+        (Order.EXPIRED, Reservation.CANCELLED),
+        (Order.CANCELLED, Reservation.CANCELLED),
+    ),
+)
+def test_set_state_sets_reservation_state(
+    two_hour_reservation, order_state, expected_reservation_state
+):
+    old_order_state = (
+        Order.CONFIRMED if order_state == Order.CANCELLED else Order.WAITING
+    )
     order = OrderFactory(reservation=two_hour_reservation, state=old_order_state)
 
     order.set_state(order_state)
@@ -40,32 +47,38 @@ def test_set_state_sets_reservation_state(two_hour_reservation, order_state, exp
     assert two_hour_reservation.state == expected_reservation_state
 
 
-@pytest.mark.parametrize('state, new_state', (
-    (Order.REJECTED, Order.CONFIRMED),
-    (Order.REJECTED, Order.EXPIRED),
-    (Order.REJECTED, Order.CANCELLED),
-    (Order.CONFIRMED, Order.REJECTED),
-    (Order.CONFIRMED, Order.EXPIRED),
-    (Order.EXPIRED, Order.REJECTED),
-    (Order.EXPIRED, Order.CONFIRMED),
-    (Order.EXPIRED, Order.CANCELLED),
-    (Order.CANCELLED, Order.REJECTED),
-    (Order.CANCELLED, Order.CONFIRMED),
-    (Order.CANCELLED, Order.EXPIRED),
-    (Order.WAITING, Order.CANCELLED),
-))
+@pytest.mark.parametrize(
+    "state, new_state",
+    (
+        (Order.REJECTED, Order.CONFIRMED),
+        (Order.REJECTED, Order.EXPIRED),
+        (Order.REJECTED, Order.CANCELLED),
+        (Order.CONFIRMED, Order.REJECTED),
+        (Order.CONFIRMED, Order.EXPIRED),
+        (Order.EXPIRED, Order.REJECTED),
+        (Order.EXPIRED, Order.CONFIRMED),
+        (Order.EXPIRED, Order.CANCELLED),
+        (Order.CANCELLED, Order.REJECTED),
+        (Order.CANCELLED, Order.CONFIRMED),
+        (Order.CANCELLED, Order.EXPIRED),
+        (Order.WAITING, Order.CANCELLED),
+    ),
+)
 def test_set_state_denied_transitions(two_hour_reservation, state, new_state):
     order = OrderFactory(reservation=two_hour_reservation, state=state)
     with pytest.raises(OrderStateTransitionError):
         order.set_state(new_state)
 
 
-@pytest.mark.parametrize('state, new_state', (
-    (Order.WAITING, Order.CONFIRMED),
-    (Order.WAITING, Order.EXPIRED),
-    (Order.WAITING, Order.REJECTED),
-    (Order.CONFIRMED, Order.CANCELLED),
-))
+@pytest.mark.parametrize(
+    "state, new_state",
+    (
+        (Order.WAITING, Order.CONFIRMED),
+        (Order.WAITING, Order.EXPIRED),
+        (Order.WAITING, Order.REJECTED),
+        (Order.CONFIRMED, Order.CANCELLED),
+    ),
+)
 def test_set_state_allowed_transitions(two_hour_reservation, state, new_state):
     order = OrderFactory(reservation=two_hour_reservation, state=state)
     order.set_state(new_state)
