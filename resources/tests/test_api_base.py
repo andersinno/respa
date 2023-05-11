8804d8feb6a85a93e78443c3edcbf15d8c5917d2
Dan Jacob <dan.jacob@anders.com>
2023-05-11 16:15:02 +0300

    Resources: tidy up test_api_base.py
    Code formatted with isort/Black.
    
    Refs TTVA-59

--- resources/tests/test_api_base.py
+++ resources/tests/test_api_base.py
@@ -11,7 +11,8 @@ def TMS():
     class TestSerializer(TranslatedModelSerializer):
         class Meta:
             model = ResourceEquipment
-            fields = '__all__'
+            fields = "__all__"
+
     return TestSerializer
 
 
@@ -29,29 +30,33 @@ def test_translated_model_serializer_language_filtering(TMS, equipment, space_re
     resource_equipment = ResourceEquipment.objects.create(
         equipment=equipment,
         resource=space_resource,
-        description_fi='testiresurssissa olevan testivarusteen kuvaus',
-        description_en=''
+        description_fi="testiresurssissa olevan testivarusteen kuvaus",
+        description_en="",
     )
     representation = tms.to_representation(resource_equipment)
-    description = representation['description']
-    assert description['fi'] == 'testiresurssissa olevan testivarusteen kuvaus'
-    assert 'en' not in description  # empty field should not be included
-    assert 'sv' not in description  # null field should not be included
+    description = representation["description"]
+    assert description["fi"] == "testiresurssissa olevan testivarusteen kuvaus"
+    assert "en" not in description  # empty field should not be included
+    assert "sv" not in description  # null field should not be included
 
     resource_equipment_descriptions_null = ResourceEquipment.objects.create(
         equipment=equipment,
         resource=space_resource,
     )
     representation = tms.to_representation(resource_equipment_descriptions_null)
-    assert representation['description'] is None  # should be None as all description fields are null
+    assert (
+        representation["description"] is None
+    )  # should be None as all description fields are null
 
     resource_equipment_descriptions_empty = ResourceEquipment.objects.create(
         equipment=equipment,
         resource=space_resource,
-        description_fi='',
-        description_en='',
-        description_sv='',
+        description_fi="",
+        description_en="",
+        description_sv="",
     )
 
     representation = tms.to_representation(resource_equipment_descriptions_empty)
-    assert representation['description'] is None  # should be None as all description fields are empty
+    assert (
+        representation["description"] is None
+    )  # should be None as all description fields are empty
