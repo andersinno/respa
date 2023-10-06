3104f387842418f9441cf453c7957c7c2a66c12e
Joonas Syysvirta <joonas.syysvirta@anders.fi>
2023-10-06 12:36:54 +0300

    Respa Admin: Add view for managing invoiceable reservations
    Add a list view to Respa Admin that shows all invoiceable
    reservations. This is the view where admins can mark the reservation
    as ready to be sent to SAP for invoicing. That will be implemented
    in a follow-up commit.
    
    Refs TTVA-161

---
+++ respa_admin/views/reservations.py
@@ -0,0 +1,45 @@
+from django.db.models import FieldDoesNotExist, Q
+from django.views.generic import ListView
+
+from resources.models import Reservation, Resource
+from respa_admin.views.base import ExtraContextMixin
+
+
+class InvoiceableReservationListView(ExtraContextMixin, ListView):
+    model = Reservation
+    paginate_by = 10
+    context_object_name = "reservations"
+    template_name = "respa_admin/page_invoiceable_reservations.html"
+
+    def get(self, request, *args, **kwargs):
+        get_params = request.GET
+        self.search_query = get_params.get("search_query")
+        self.order_by = get_params.get("order_by", "name")
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["search_query"] = self.search_query or ""
+        context["order_by"] = self.order_by
+        return context
+
+    def get_queryset(self):
+        managed_resources = Resource.objects.modifiable_by(self.request.user)
+        qs = Reservation.objects.filter(
+            invoice_requested=True, resource__in=managed_resources
+        ).select_related("order", "resource")
+
+        if self.search_query:
+            qs = qs.filter(
+                Q(resource__name__icontains=self.search_query)
+                | Q(company__icontains=self.search_query)
+                | Q(reserver_id__icontains=self.search_query)
+            )
+        if self.order_by:
+            order_by_param = self.order_by.strip("-")
+            try:
+                if Reservation._meta.get_field(order_by_param):
+                    qs = qs.order_by(self.order_by)
+            except FieldDoesNotExist:
+                pass
+        return qs
