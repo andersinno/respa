4b218e51f9e552573c1510922b0df032c23b3ac4
Joonas Syysvirta <joonas.syysvirta@anders.fi>
2024-01-04 10:38:40 +0200

    Allow bigger page size for Reservation API endpoint for Excel/CSV reports
    Allow a page size of 50000 records when getting the reservations
    in Excel/CSV format. This allows downloading a report of more
    than the 500 row limit.
    
    Refs TTVA-191

--- resources/pagination.py
+++ resources/pagination.py
@@ -3,7 +3,9 @@ from rest_framework.pagination import PageNumberPagination, _positive_int
 
 class DefaultPagination(PageNumberPagination):
     page_size = 20
-    page_size_query_param = 'page_size'  # Allow client to override, using `?page_size=xxx
+    page_size_query_param = (
+        "page_size"  # Allow client to override, using `?page_size=xxx
+    )
     max_page_size = 500
 
 
@@ -15,12 +17,18 @@ class ReservationPagination(DefaultPagination):
     def get_page_size(self, request):
         if self.page_size_query_param:
             cutoff = self.max_page_size
-            if request.query_params.get('format', '').lower() == 'xlsx':
+            if request.query_params.get("format", "").lower() == "xlsx":
+                cutoff = 50000
+            if request.accepted_media_type in [
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "text/csv",
+            ]:
                 cutoff = 50000
             try:
                 return _positive_int(
                     request.query_params[self.page_size_query_param],
-                    strict=True, cutoff=cutoff
+                    strict=True,
+                    cutoff=cutoff,
                 )
             except (KeyError, ValueError):
                 pass
